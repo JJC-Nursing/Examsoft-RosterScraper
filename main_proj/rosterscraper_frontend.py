b65473ae86f2c7f237795d3b26a34be4aa950436
Jefferson C <python-dev@yjefecustom.me>
2023-04-06 19:12:55 -0500

    This is v1.01 of front end. Still disconnected to back end, but color gradients work.

--- main_proj/rosterscraper_frontend.py
+++ main_proj/rosterscraper_frontend.py
@@ -1,15 +1,23 @@
-#!/usr/bin/env python3
-
 # future updates: a date / time appender to file export (easy way to timestamp)
+#                 a remembrance of where the program last saved and what its name was last (check for overwriting)
 
+txt_version = "v1.1"
+
+txt_updatedate = "03.30.2023"
+
+txt_stuff = "script orig. written by Jefferson Cherrington" + "\n\n" + \
+            "creates a CSV roster from a particularly formatted HTML file" + "\n" + \
+            "(all scraping is done offline)" + "\n\n" + \
+            "last updated: " + txt_updatedate + "\n"
 
 import sys
 import rosterscraper_backend as esv
 from pathlib import Path
 
-from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QInputDialog, QLabel, QVBoxLayout, QGridLayout, QWidget
-from PyQt6.QtGui import QFont, QFontDatabase
+from PyQt6.QtCore import QSize, Qt, QAbstractAnimation, QVariantAnimation
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QInputDialog, QLabel, QVBoxLayout, \
+    QGridLayout, QWidget
+from PyQt6.QtGui import QFont, QFontDatabase, QColor, QCursor
 
 # Only needed for access to command line arguments
 import sys
@@ -20,9 +28,9 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.initUI()
+        self.init_ui()
 
-    def initUI(self):
+    def init_ui(self):
 
         fonts_name = [QFontDatabase.addApplicationFont("fonts/BeVietnam-ExtraBold.ttf"),
                       QFontDatabase.addApplicationFont("fonts/BeVietnam-Regular.ttf"),
@@ -38,35 +46,51 @@ class MainWindow(QMainWindow):
         # label.setFont(QFont("Be Vietnam ExtraBold", 20))
         # label.move(50, 50)
 
-        self.setWindowTitle("My App")
+        self.setWindowTitle("RosterScraper Start Screen")
         btn_import = QPushButton("import location")
         # btn_import.setCheckable(True)
-        btn_import.setFont(QFont("Be Vietnam", 10))
+        btn_import.setFont(QFont("Be Vietnam", 12))
         btn_import.released.connect(self.import_location)
+        btn_import.setStyleSheet(".QPushButton{"
+                                 "background: qlineargradient(spread:pad, x1:0 y1:0, x2:1 y2:0, "
+                                 "stop:0 rgba(0, 0, 0, 20), stop:1 rgba(255, 255, 255, 60));"
+                                 "border: 2px solid white; border-radius: 8.5px;"
+                                 "padding: 5px;"
+                                 "}"
+                                 ".QPushButton:hover{"
+                                 "background: rgba(0, 165, 180, 70);"
+
+                                 "}"
+                                 )
 
         lbl_title = QLabel("Examsoft GUI Test", self)
         lbl_title.setFont(QFont("Be Vietnam ExtraBold", 20))
         lbl_title.setAlignment(Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignLeft)
 
-        lbl_stuff = QLabel("Lorem ipsum decorum.", self)
+        lbl_stuff = QLabel(txt_stuff, self)
         lbl_stuff.setFont(QFont("Be Vietnam", 10))
         # lbl_stuff.setAlignment(Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignLeft)
 
         layout_words = QVBoxLayout()
         layout_words.addWidget(lbl_title)
         layout_words.addWidget(lbl_stuff)
+        #  layout_words.setStyleSheet()
 
         layout_main = QVBoxLayout()
         layout_main.addLayout(layout_words)
         layout_main.addWidget(btn_import)
 
         container = QWidget()
+        container.setStyleSheet(".QWidget "
+                                "{background: "
+                                "qlineargradient(x1:0 y1:1, x2:0.5 y2:0, stop:0 #23002A, stop:1 #00066A);}"
+                                "* {color: #FFFFFF};")
         container.setLayout(layout_main)
 
         # Set the central widget of the Window.
         self.setCentralWidget(container)
 
-        self.setMinimumSize(QSize(400, 115))
+        self.setMinimumSize(QSize(400, 110))
 
         # Set the central widget of the Window.
         # self.setCentralWidget(btn_import)
